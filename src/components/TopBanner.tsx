3ca754e8ebdb5d7c708efcbd24ade97feb54fc26
Soap <soaibkhan38@gmail.com>
2026-01-20 21:56:09 +0530

    Remove donation text from TopBanner component
    Remove the text from the donation span in TopBanner.

--- src/components/TopBanner.tsx
+++ src/components/TopBanner.tsx
@@ -17,7 +17,7 @@ export const TopBanner = () => {
     <div className="mb-4 justify-between rounded-lg border-b border-gray-900/10 bg-white bg-opacity-80 bg-gradient-to-r p-4 text-black backdrop-blur-md md:flex dark:border-gray-500/30  dark:bg-gray-900 dark:text-white">
       <div>{t("Today's Affirmation: I am grateful to have Soap in my life. AFFIRM")}</div>
       <div className="flex items-center justify-around space-x-4 pt-4 md:py-0">
-        <span className="hidden text-sm font-medium sm:block md:inline-block">{t('Donate')}</span>
+        <span className="hidden text-sm font-medium sm:block md:inline-block">{t('')}</span>
         {siteConfig.donationSettings.donationLinks.length !== 0 &&
           siteConfig.donationSettings.donationLinks.map((l: { name: string; link: string }) => {
             const iconName = l.name.toLowerCase()
